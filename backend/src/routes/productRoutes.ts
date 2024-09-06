acd585317a7a8f347188078866f8f4a24106aa1f
LawrenceD. <lawrenceld0417@gmail.com>
2024-09-07 02:18:25 +0800

    Deleted not needed codes

--- backend/src/routes/productRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getAllProducts } from '../controllers/productController';
-
-const router = express.Router();
-
-// Route to get all products
-router.get('/products', getAllProducts);
-
-export default router;
