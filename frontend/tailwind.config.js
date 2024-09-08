2655ccb37f311cade47c449b956f5bdc41096183
LawrenceD. <lawrenceld0417@gmail.com>
2024-09-08 14:09:02 +0800

    Basic login form

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,14 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", // Adjust paths based on where your files are located
   ],
   theme: {
-    extend: {
-    },
+    extend: {},
   },
   plugins: [
     require('@tailwindcss/forms'),
+    // Other plugins here
   ],
 }
-
